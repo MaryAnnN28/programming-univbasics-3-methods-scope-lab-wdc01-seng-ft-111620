17a4d7e01a160dcfb2b116e2ce3e6e9443f04d37
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-14 03:36:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
 
 
-def mario(phrase)
+def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-def toadstool(status)
+def toadstool
   status = 'Thank you Mario! But Our Princess Is In Another Castle!'
   puts status
 end
